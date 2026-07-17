25f2ae55f156dd9055b50f0db853090f3f66de7b
chris300mcgeough-sketch <chris300.mcgeough@gmail.com>
2026-07-17 02:24:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import streamlit as st 
-st.title("Christophers Weebsite")
+st.title("Christophers Website")
 st.write("my first website")
 name=st.text_input("Enter your name")
 
 if name:
   st.success(f"Hello {name}")
+x=10
+if x==10:
+  st.write(x)
+else:
+  st.write("else")
